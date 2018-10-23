395f593f040083a76a5f9b8642ed05aa59d24e52
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2018-10-23 08:46:24 -0500

    no aliases

--- orgInit.sh
+++ orgInit.sh
@@ -1,4 +1,4 @@
-sfdx force:org:create -f config/project-scratch-def.json -s -a vol -d 1
+sfdx force:org:create -f config/project-scratch-def.json -s -d 1
 sfdx force:source:push
 sfdx force:user:permset:assign -n VolunteeringApp
 sfdx force:data:tree:import -p data/masterImportPlan.json
