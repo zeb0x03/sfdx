481e8ce5174902bb773872dd6f085cf7fe090684
mshanemc <shane.mclaughlin@salesforce.com>
2019-12-11 12:02:45 -0600

    plugin permset:assign

--- orgInit.sh
+++ orgInit.sh
@@ -1,6 +1,6 @@
 sfdx shane:org:create -f config/project-scratch-def.json -s -d 1 --userprefix crash --userdomain course.org
 sfdx force:source:push
-sfdx force:user:permset:assign -n VolunteeringApp
+sfdx shane:user:permset:assign -n VolunteeringApp
 sfdx force:data:tree:import -p data/masterImportPlan.json
 sfdx force:apex:execute -f scripts/setup.cls
 sfdx shane:user:password:set -p sfdx1234 -g User -l User
